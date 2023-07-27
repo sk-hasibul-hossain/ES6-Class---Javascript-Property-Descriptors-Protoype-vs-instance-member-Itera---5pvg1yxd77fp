7058f0a38b78118a6d04101124845547d27bb310
Hasibul <skhasibulhossain16@gmail.com>
2023-07-27 20:31:53 +0530

    code written

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 //Write your code here
-
-
-
-
+class Api {
+  #secure;
+  constructor(url, method = "GET") {
+    this.url = url;
+    this.method = method;
+    this.#secure = url.startsWith("https");
+  }
+  isSecure() {
+    return this.#secure;
+  }
+  updateUrl(url) {
+    this.url = url;
+  }
+}
 
 //Do not remove this code
-module.exports = { API }
+module.exports = { API };
